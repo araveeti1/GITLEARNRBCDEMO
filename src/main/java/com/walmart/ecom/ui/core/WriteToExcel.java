c2784a36360b95a100cf76493ce0261da6b3fd58
Demo Scotiobank <venkatestscotioa@gmail.com>
2022-12-03 18:59:31 -0500

    COmmit

--- src/main/java/com/walmart/ecom/ui/core/WriteToExcel.java
+++ src/main/java/com/walmart/ecom/ui/core/WriteToExcel.java
@@ -15,6 +15,9 @@ import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class WriteToExcel {
+	//write
+	//read
+	//learn
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
